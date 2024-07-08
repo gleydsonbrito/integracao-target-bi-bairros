ee2775c8e501ad27eca7036085fe44e252952ec6
gleydsonbrito <gleydsonbrito@hotmail.com>
2024-07-08 20:23:58 -0300

    finaliza a implementação da tabela com filtro para os dados do traget

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-fetch('http://172.21.3.188:9047/api/v3/job/197a6a46-db56-0c1b-8856-ef93a7dbb700/results', {
-    method: 'GET', // Método da requisição
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer tqi1f3eii09ibdt0lklp81v3jh`,
-    }
-})
+fetch('https://n8n-datalakepcr.recife.pe.gov.br/webhook/target-obras')
 .then(res => res.json())
-.then(res => console.log(res.rows))
+.then(res => {
+    console.log(res)
+    res.forEach(element => {
+        console.log(element)
+    });
+})
+
+
+const trh = document.createElement('tr')
+            for(const key in bairros[0]){
+                const th = document.createElement('th')
+                th.appendChild(document.createTextNode(key))
+                trh.appendChild(th)
+            }
+            thead.appendChild(trh)

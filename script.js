65dc2c812949bea621a5f5186531e40bcf2e54e5
gleydsonbrito <gleydsonbrito@hotmail.com>
2024-07-18 15:58:53 -0300

    ajusta a requisição e remove o cacheamento local: armazenamento local não tem espaço

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 fetch('https://n8n-datalakepcr.recife.pe.gov.br/webhook/target-obras')
 .then(res => res.json())
 .then(res => {
-    console.log(res)
-    res.forEach(element => {
-        console.log(element)
-    });
+    console.log(res.length)
 })
-
-
-const trh = document.createElement('tr')
-            for(const key in bairros[0]){
-                const th = document.createElement('th')
-                th.appendChild(document.createTextNode(key))
-                trh.appendChild(th)
-            }
-            thead.appendChild(trh)

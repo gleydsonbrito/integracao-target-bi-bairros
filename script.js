8130d680fafb64c468eb19115e62949c070af23b
gleydsonbrito <gleydsonbrito@hotmail.com>
2024-08-09 16:14:09 -0300

    remove a tabela inicial

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+/*
 fetch('https://n8n-datalakepcr.recife.pe.gov.br/webhook/target-obras')
 .then(res => res.json())
 .then(res => {
     console.log(res.length)
-})
+})*/
+
+let a = {
+    1: "um"
+}
+
+if(a[2]) {
+    console.log(`tem a chave`)
+}
